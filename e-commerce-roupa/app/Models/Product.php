6177250295b404373895e0002fd72bef336a35ef
caua075 <caua.andradeg07@gmail.com>
2025-03-13 19:28:12 -0300

    insertion the models and migrations of products and catgories

---
+++ e-commerce-roupa/app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+    protected $fillable = ['name', 'description', 'price', 'stock', 'size', 'category_id',];
+    
+}
