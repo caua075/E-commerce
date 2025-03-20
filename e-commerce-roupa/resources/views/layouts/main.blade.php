8488cf7c5001f3380dbc62400d80f854858132bd
caua075 <caua.andradeg07@gmail.com>
2025-03-20 16:47:05 -0300

    styling auth

--- e-commerce-roupa/resources/views/layouts/main.blade.php
+++ e-commerce-roupa/resources/views/layouts/main.blade.php
@@ -35,12 +35,13 @@
   <symbol id="search"viewBox="0 0 16 16">
     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
   </symbol>
-
-  <symbol id="loc" viewBox="0 0 16 16">
-    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
-    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+  <symbol id="loc" class="bi bi-geo-alt" viewBox="0 0 16 16">
+  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
   </symbol>
 </svg>
+
+
 
 <body>
   <div class="container">
@@ -94,9 +95,11 @@
       <nav class="d-flex col-12 col-lg-auto mb-2 justify-content-center mb-md-0">
         <a href="/" class="nav-link px-2 link-dark">Home</a>
       </nav>
+      @if(Auth::check() && Auth::user()->admin)
       <nav class="d-flex col-12 col-lg-auto mb-2 justify-content-center mb-md-0">
-        <a href="/products/create" class="nav-link px-2 link-dark">Produtos</a>
+        <a href="/products/create" class="nav-link px-2 link-dark">Cadastrar Produtos</a>
       </nav>
+      @endif
     </header>
   </div>
     <main class="container">
@@ -129,7 +132,6 @@
         </footer>
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
@@ -141,3 +143,4 @@
   }
 </script>
 </html>
+
---
+++ e-commerce-roupa/resources/views/layouts/mainLogin.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="E-commerce de roupas com os melhores preços e qualidade.">
+    <meta name="keywords" content="roupas, e-commerce, moda, masculino">
+    <title>Gabaldi MensWear</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
+
+    <!-- CSS only -->
+    <link rel="stylesheet" href="/css/styleLogin.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+    @yield('content')
+    <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
